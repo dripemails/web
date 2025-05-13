6e1224595858083f6d133fe178310cfd2bae246b
dripemails <founders@dripemails.org>
2025-05-12 23:42:08 -0500

    Update URL configurations

--- dripemails/urls.py
+++ dripemails/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView # This was in your original file
 from django.conf.urls.i18n import i18n_patterns
 
 # URL patterns that should NOT be prefixed with a language code
@@ -12,13 +11,12 @@ non_prefixed_urlpatterns = [
 ]
 
 # URL patterns that SHOULD be prefixed with a language code
-# Note: 'prefix_default_language=False' means the default language won't have a prefix (e.g. /en/)
 language_prefixed_urlpatterns = i18n_patterns(
     path('accounts/', include('allauth.urls')),
-    path('api/', include('campaigns.urls')),
-    path('api/', include('subscribers.urls')),
-    path('api/', include('analytics.urls')),
-    path('', include('core.urls')), # Handles the homepage and other core app views
+    path('', include('core.urls')),
+    path('', include('campaigns.urls')),
+    path('', include('subscribers.urls')),
+    path('', include('analytics.urls')),
     prefix_default_language=False
 )
 

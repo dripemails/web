cea3b2dc5e5eca7572138be31bff432f5139d2e1
Aidan Turgut <aidantur@gmail.com>
2025-10-26 11:49:59 -0500

    added an analysis dashboard web page

---
+++ campaignAnalysis.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+import pandas as pd 
+
+st.title("Here's the latest on campaign: (campaign name)")
+numViewsData={
+    'Date': ['January 2025', 'February 2025', 'March 2025'],
+    'Views': [200, 300, 500]
+}
+df1=pd.DataFrame(numViewsData)
+behaviorsData={
+    'Behavior': ['Clicking on ad', 'Ignoring ad'],
+    'Number of people': [200, 400]
+}
+df2=pd.DataFrame(behaviorsData)
+numViews, behaviors=st.columns(2)
+
+with numViews:
+    st.subheader("Number of views")
+    st.bar_chart(df1.set_index('Date'))
+with behaviors:
+    st.subheader("Behavior analytics")
+    st.bar_chart(df2.set_index('Behavior'))

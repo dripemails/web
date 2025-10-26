d6f61bfd0f56739a71fcc644a1234db83876add2
Aidan Turgut <aidantur@gmail.com>
2025-10-26 12:07:21 -0500

    added email footer analysis to the dashboard website

--- campaignAnalysis.py
+++ campaignAnalysis.py
@@ -11,12 +11,18 @@ behaviorsData={
     'Behavior': ['Clicking on ad', 'Ignoring ad'],
     'Number of people': [200, 400]
 }
+footerData={
+    'Link': ['Privacy policy link', 'Main site link'],
+    'Number of people': [200, 400]
+}
 df2=pd.DataFrame(behaviorsData)
 numViews, behaviors=st.columns(2)
-
+df3=pd.DataFrame(footerData)
 with numViews:
     st.subheader("Number of views")
     st.bar_chart(df1.set_index('Date'))
 with behaviors:
     st.subheader("Behavior analytics")
     st.bar_chart(df2.set_index('Behavior'))
+st.subheader("Email footer analytics")
+st.bar_chart(df3.set_index('Link'))

76348e574d58c2e2cde780f8fb1051485901b04a
dripemails <founders@dripemails.org>
2025-11-18 13:08:29 -0600

    removed password from the database

--- smtp_server/docs/asgi_settings.py
+++ smtp_server/docs/asgi_settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME', 'dripemails'),
         'USER': os.environ.get('DB_USER', 'dripemails'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'fender45'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '3306'),
         'OPTIONS': {

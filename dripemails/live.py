cefc9f6b66266cd27b91c5347c32845c6df426e6
dripemails <founders@dripemails.org>
2025-11-24 11:29:09 -0600

    fixed redis backend

--- dripemails/live.py
+++ dripemails/live.py
@@ -319,9 +319,6 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/1'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        },
         'KEY_PREFIX': 'dripemails',
         'TIMEOUT': 300,  # 5 minutes default
     }
